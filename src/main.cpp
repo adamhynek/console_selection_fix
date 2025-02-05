6dad36e5f711821bfa3a70d946d32038bfcb511e
Adam Hynek <adamhynek@gmail.com>
2025-02-05 01:09:41 -0800

    completely reimplement everything

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "skse64_common/Relocation.h"
 #include "skse64_common/BranchTrampoline.h"
 
+#include <DirectXMath.h>
+
 #include <ShlObj.h>  // CSIDL_MYDOCUMENTS
 
 #include "version.h"
@@ -17,10 +19,37 @@ static SKSEMessagingInterface *g_messaging = nullptr;
 SKSETrampolineInterface *g_trampoline = nullptr;
 
 
+RelocPtr<float> fWindowCutoutSize_VR(0x1EABDF8);
+
+
 namespace RE {
+    struct ViewData
+    {
+        DirectX::XMVECTOR m_ViewUp; // 00
+        DirectX::XMVECTOR m_ViewRight; // 10
+        DirectX::XMVECTOR m_ViewForward; // 20
+        DirectX::XMMATRIX m_ViewMat; // 30
+        DirectX::XMMATRIX m_ProjMat; // 70
+        DirectX::XMMATRIX m_ViewProjMat; // B0
+        DirectX::XMMATRIX m_UnknownMat1; // F0 - all 0?
+        DirectX::XMMATRIX m_ViewProjMatrixUnjittered; // 130
+        DirectX::XMMATRIX m_PreviousViewProjMatrixUnjittered; // 170
+        DirectX::XMMATRIX m_ProjMatrixUnjittered; // 1B0
+        DirectX::XMMATRIX m_UnknownMat2; // 1F0 - all 0?
+        float m_ViewPort[4];// 230 - NiRect<float> { left = 0, right = 1, top = 1, bottom = 0 }
+        NiPoint2 m_ViewDepthRange; // 240
+        char _pad0[0x8]; // 248
+    }; // size == 250
+
+    struct RendererShadowState
+    {
+        UInt8 unk00[0x3E0 - 0x000];
+        ViewData m_CameraData[2]; // 3E0 - size of each is 250
+    };
+
     struct NiCamera : NiAVObject
     {
-        float			m_aafWorldToCam[4][4];	// 110
+        float m_aafWorldToCam[4][4];	// 110
         NiFrustum *ViewFrustumPtr_178;
         tArray<NiFrustum> eyeFrustums; // 180
         tArray<NiPoint3> eyePositions; // 198
@@ -34,23 +63,66 @@ namespace RE {
     };
 }
 
+RelocPtr<RE::RendererShadowState> g_rendererShadowState(0x3180DB0);
 
-typedef void (*_NiCamera_ConsolePick_GetStartAndDir)(RE::NiCamera *camera, float a_rightAmt, float a_upAmt, NiPoint3 &a_camPosOut, NiPoint3 &a_dirOut);
-_NiCamera_ConsolePick_GetStartAndDir NiCamera_ConsolePick_GetStartAndDir_Original = nullptr;
-auto NiCamera_ConsolePick_GetStartAndDir_HookLoc = RelocPtr<uintptr_t>(0xCAB37F);
-void NiCamera_ConsolePick_GetStartAndDir_Hook(RE::NiCamera *camera, float a_rightAmt, float a_upAmt, NiPoint3 &a_camPosOut, NiPoint3 &a_dirOut)
+
+NiPoint3 ScreenToWorldDirection(float x_ndc, float y_ndc, const DirectX::XMMATRIX &viewProjectionMatrix) {
+    // Step 1: Convert screen coordinates to Normalized Device Coordinates (NDC)
+    float z_ndc = 1.0f; // Forward direction
+
+    // Step 2: Define near and far clip space coordinates
+    DirectX::XMVECTOR nearPoint = DirectX::XMVectorSet(x_ndc, y_ndc, 0.0f, 1.0f); // z = 0 for near plane
+    DirectX::XMVECTOR farPoint = DirectX::XMVectorSet(x_ndc, y_ndc, 1.0f, 1.0f); // z = 1 for far plane
+
+    // Step 3: Invert the View-Projection matrix
+    DirectX::XMMATRIX invVP = DirectX::XMMatrixInverse(nullptr, viewProjectionMatrix);
+
+    // Step 4: Unproject to world space
+    DirectX::XMVECTOR nearWorld = XMVector4Transform(nearPoint, invVP);
+    DirectX::XMVECTOR farWorld = XMVector4Transform(farPoint, invVP);
+
+    // Step 5: Perspective divide to convert from homogeneous coordinates
+    nearWorld = DirectX::XMVectorScale(nearWorld, 1.0f / DirectX::XMVectorGetW(nearWorld));
+    farWorld = DirectX::XMVectorScale(farWorld, 1.0f / DirectX::XMVectorGetW(farWorld));
+
+    // Step 6: Get the direction vector and normalize it
+    DirectX::XMVECTOR direction = DirectX::XMVector3Normalize(DirectX::XMVectorSubtract(farWorld, nearWorld));
+
+    return { direction.m128_f32[0], direction.m128_f32[1], direction.m128_f32[2] };
+}
+
+
+typedef bool (*_NiCamera_GetStartAndDirFromScreenRectCoords)(RE::NiCamera *camera, int midpointX, int midpointY, NiPoint3 &a_camPosOut, NiPoint3 &a_dirOut, float uiWidth, float uiHeight);
+_NiCamera_GetStartAndDirFromScreenRectCoords NiCamera_GetStartAndDirFromScreenRectCoords_Original = nullptr;
+auto NiCamera_GetStartAndDirFromScreenRectCoords_HookLoc = RelocPtr<uintptr_t>(0x885574);
+bool NiCamera_GetStartAndDirFromScreenRectCoords_Hook(RE::NiCamera *camera, int mouseX, int mouseY, NiPoint3 &a_camPosOut, NiPoint3 &a_dirOut, float uiWidth, float uiHeight)
 {
-    //_MESSAGE("%.2f %2f", a_rightAmt, a_upAmt);
+    if (!NiCamera_GetStartAndDirFromScreenRectCoords_Original(camera, mouseX, mouseY, a_camPosOut, a_dirOut, uiWidth, uiHeight)) {
+        // It checks stuff like if the mouse is outside the port, so we don't need to re-check that here
+        return false;
+    }
 
-    a_upAmt *= Config::options.upMultiplier;
-    a_rightAmt *= Config::options.rightMultiplier;
+    float xPercent = mouseX / uiWidth;
+    float yPercent = 1.f - (mouseY / uiHeight);
 
-    a_upAmt += Config::options.upOffset * Config::options.upMultiplier;
-    a_rightAmt += Config::options.rightOffset * Config::options.rightMultiplier;
+    _MESSAGE("%.3f %.3f", xPercent, yPercent);
 
-    NiCamera_ConsolePick_GetStartAndDir_Original(camera, a_rightAmt, a_upAmt, a_camPosOut, a_dirOut);
+    RE::RendererShadowState *rendererShadowState = g_rendererShadowState;
 
-    a_camPosOut += camera->eyePositions.entries[0] - camera->m_worldTransform.pos;
+    float rightAmount = (xPercent - camera->port.m_left) / (camera->port.m_right - camera->port.m_left);
+    float upAmount = (yPercent - camera->port.m_bottom) / (camera->port.m_top - camera->port.m_bottom);
+    rightAmount = 2.f * rightAmount - 1.f;
+    upAmount = 2.f * upAmount - 1.f;
+    rightAmount *= (1.f - 4.f * *fWindowCutoutSize_VR);
+    upAmount *= (1.f - 2.f * *fWindowCutoutSize_VR);
+
+    _MESSAGE("%.3f %.3f", rightAmount, upAmount);
+
+    a_dirOut = ScreenToWorldDirection(rightAmount, upAmount, *(DirectX::XMMATRIX *)&rendererShadowState->m_CameraData[0].m_ViewProjMatrixUnjittered);
+
+    a_camPosOut = camera->eyePositions.entries[0];
+
+    return true;
 }
 
 typedef bool(*_CollectPickResults_NiNode)(NiPoint3 *a_start, NiPoint3 *a_dir, void *a3, NiNode *a4);
@@ -81,8 +153,8 @@ std::uintptr_t Write5Call(std::uintptr_t a_src, std::uintptr_t a_dst)
 void DoHooks()
 {
     {
-        std::uintptr_t originalFunc = Write5Call(NiCamera_ConsolePick_GetStartAndDir_HookLoc.GetUIntPtr(), uintptr_t(NiCamera_ConsolePick_GetStartAndDir_Hook));
-        NiCamera_ConsolePick_GetStartAndDir_Original = (_NiCamera_ConsolePick_GetStartAndDir)originalFunc;
+        std::uintptr_t originalFunc = Write5Call(NiCamera_GetStartAndDirFromScreenRectCoords_HookLoc.GetUIntPtr(), uintptr_t(NiCamera_GetStartAndDirFromScreenRectCoords_Hook));
+        NiCamera_GetStartAndDirFromScreenRectCoords_Original = (_NiCamera_GetStartAndDirFromScreenRectCoords)originalFunc;
     }
 
     {
